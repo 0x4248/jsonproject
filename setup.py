1d1af15526bb44c7fda95c4afd024886cd9217a1
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2022-04-09 15:58:40 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+#This is to include the readme from the github repo
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='Proactive-JSONconfig',
+    url='https://github.com/Proactive-Development/JSONconfig/',
+    author='Proactive Development',
+    packages=['JSONconfig'],
+    install_requires=[''],
+    version='0.1.0',
+    license='NONE',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='Easily manage your projects with JSON files',
+)
